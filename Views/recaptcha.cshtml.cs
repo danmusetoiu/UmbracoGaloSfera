ec8958a45381a9686fa1b6b0da4e5415dc56f0eb
Dan MUSETOIU <dmusetoiu@afir.info>
2024-09-04 11:14:18 +0300

    Grupuri de membri, restrictionare acces la diferite pagini pentru useri din grupuri, diferite fix-uri design....

---
+++ Views/recaptcha.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UmbracoGaloSfera.Views
+{
+    public class recaptchaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ _AppStart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UmbracoGaloSfera
+{
+    public class _AppStartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
