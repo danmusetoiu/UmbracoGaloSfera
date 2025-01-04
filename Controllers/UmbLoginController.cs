1aa24ace90a56f591e987f253b7f4a977e804279
Dan MUSETOIU <dmusetoiu@afir.info>
2025-01-04 12:04:29 +0200

    logout

---
+++ Controllers/UmbLoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Umbraco.Cms.Web.Common.Security;
+using Umbraco.Cms.Core.Security;
+
+
+public class UmbLoginController : Controller
+{
+    private readonly IMemberSignInManager _memberSignInManager;
+
+    public UmbLoginController(IMemberSignInManager memberSignInManager)
+    {
+        _memberSignInManager = memberSignInManager;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Logout()
+    {
+        await _memberSignInManager.SignOutAsync();
+        return Redirect("/");
+    }
+}
